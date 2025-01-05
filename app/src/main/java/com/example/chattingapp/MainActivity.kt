0dc4c56d81f610064c1bc6d4cb5231354e842101
Vita Reminnyk <vitarm1818@gmail.com>
2025-01-05 15:52:45 +0100

    Splash Screen

--- app/src/main/java/com/example/chattingapp/MainActivity.kt
+++ app/src/main/java/com/example/chattingapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.chattingapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
